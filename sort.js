899f98a71f4122ffe785589ea7e0ed0d4ba655c3
Ali <75635207+AliProduction@users.noreply.github.com>
2022-01-04 20:34:32 +0100

    Add files via upload

---
+++ sort.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let path = require("path");
+let currentPath = "C:/Users/fatos/Downloads/"; 
+
+fs.readdir(currentPath, (err, files) => {
+   files.forEach(processFile);
+});
+
+function processFile(file) {
+   let endung = path.extname(file);
+   console.log(file + " Endung: " + endung);
+
+   if (file == "sort" || endung == ".js") {
+      console.log("( sort.js ) File wurde ignoriert !");
+   } else {
+      fs.mkdir(currentPath + endung, () => {
+         console.log("Ein Ordner für die Endung " + endung + " wurde erstellt !");
+   
+         fs.rename(currentPath + file, currentPath + endung + `/${file}`, () => {
+            console.log(`Die Datei ${file} wurde zu ihrer Endung ${endung} verschoben !`);
+         });
+      });
+   }
+}
+
+
